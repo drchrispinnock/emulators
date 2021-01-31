f055fed501a8e8e4d44503cf535da3793e09302e
Chris Pinnock <chris.pinnock@gmail.com>
2021-01-31 15:25:52 +0000

    graphics

---
+++ NetBSDEmulation/run-arm64.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+GRAPHICS="-nographic"
+#GRAPHICS="-device ramfb -device nec-usb-xhci,id=xhci -device usb-mouse,bus=xhci.0 -device usb-kbd,bus=xhci.0"
+
+cd $HOME/Qemu/NetBSD/arm64
+qemu-system-aarch64 -M virt -cpu cortex-a53 -smp 4 -m 4g \
+      -drive if=none,file=arm64.img,id=hd0 -device virtio-blk-device,drive=hd0 \
+      -netdev type=user,id=net0 -device virtio-net-device,netdev=net0,mac=00:11:22:33:44:55 \
+      -bios QEMU_EFI.fd $GRAPHICS
