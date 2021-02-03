980a26a455d237ccd8124ce1c944fa17392b1554
Chris Pinnock <chris.pinnock@gmail.com>
2021-02-03 08:20:55 +0000

    fix macppc again...

--- run-qemu-vm.sh
+++ run-qemu-vm.sh
@@ -37,29 +37,14 @@ case $ARCH in
 		i386)
 		QEMUFLAGS="-m 256M -hda $IMAGE -net user -net nic"
 		;;
+	arm64)
+		 CURSES="-nographic"
+		ALTCURSES="-device ramfb -device nec-usb-xhci,id=xhci -device usb-mouse,bus=xhci.0 -device usb-kbd,bus=xhci.0"
+		EMU=aarch64
+		;;
 	*)
 		;;
 esac
-
-case $OS in
-	NetBSD)
-		case $ARCH in
-			macppc)
-				EMU="ppc"
-				ISO=`cat usemeasroot.txt`
-		#			QEMUFLAGS="-prom-env boot-device=cd:,\\ofwboot.xcf\ -prom-env boot-file=netbsd.macppc -prom-env boot-args=-a -boot d -cdrom $ISO -hda $IMAGE" # - we need a CD image with the kernel & booted on it for this
-				QEMUFLAGS="-boot d -prom-env boot-device=cd:,\\ofwboot.xcf -prom-env boot-file=notfound -cdrom $ISO -net user -net nic $IMAGE"
-				;;
-			arm64)
-			 CURSES="-nographic"
-			ALTCURSES="-device ramfb -device nec-usb-xhci,id=xhci -device usb-mouse,bus=xhci.0 -device usb-kbd,bus=xhci.0"
-			EMU=aarch64
-			 QEMUFLAGS="-M virt -cpu cortex-a53 -smp 4 -m 4g -drive if=none,file=$IMAGE,id=hd0 -device virtio-blk-device,drive=hd0 -netdev type=user,id=net0 -device virtio-net-device,netdev=net0,mac=00:11:22:33:44:55 -bios QEMU_EFI.fd"
-						;;
-						*)
-						;;
-					esac
-				esac
 						
 if [ -n "$3" ]; then
 	CURSES=$ALTCURSES
@@ -79,6 +64,26 @@ if [ "$?" != "0" ]; then
 	echo "Error creating and changing to the target directory">&2
 	exit 1
 fi
+
+case $OS in
+	NetBSD)
+		case $ARCH in
+			macppc)
+				EMU="ppc"
+				ISO=`cat usemeasroot.txt`
+
+# Annoyingly -a does not get passed to the bootloader
+#				QEMUFLAGS="-prom-env boot-device=cd:,\\ofwboot.xcf\ -prom-env boot-file=netbsd.macppc -prom-env boot-args=-a -boot d -cdrom $ISO -hda $IMAGE" # - we need a CD image with the kernel & booted on it for this
+# We do this to drop to Boot: which understands "netbsd.macppc -a"
+			QEMUFLAGS="-boot d -prom-env boot-device=cd:,\\ofwboot.xcf -prom-env boot-file=notfound -cdrom $ISO -net user -net nic $IMAGE"
+				;;
+			arm64)
+			 QEMUFLAGS="-M virt -cpu cortex-a53 -smp 4 -m 4g -drive if=none,file=$IMAGE,id=hd0 -device virtio-blk-device,drive=hd0 -netdev type=user,id=net0 -device virtio-net-device,netdev=net0,mac=00:11:22:33:44:55 -bios QEMU_EFI.fd"
+						;;
+						*)
+						;;
+					esac
+				esac
 
 if [ -f "$IMAGE" ]; then
 
