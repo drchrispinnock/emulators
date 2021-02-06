20fa735ba4eaa257775f4656bcb05f17588c226d
Chris Pinnock <chris.pinnock@gmail.com>
2021-02-06 16:08:03 +0000

    Plan9 support first cut

--- use-qemu-vm.sh
+++ use-qemu-vm.sh
@@ -10,12 +10,13 @@
 # FreeBSD - i386, amd64, sparc64
 # Debian - amd64
 # Solaris - i386
+# Plan9 - i386
 
 # Usage: $0 [[[[[OS] Arch] NOGUI] Size]
 # e.g.
 # $0 [-i] OpenBSD i386 - run the installer
 # $0 OpenBSD i386 - run the VM or the installer if it isn't setup
-USAGE="$0 [-i][-c] [OS [arch [ver]]]\n  -i run installer ISO\n  -c use -display curses\n"
+USAGE="$0 [-i] [-c] [-n] [-d] [-t TargetDir] [OS [arch [ver]]]\n  -i run installer ISO\n  -c use -display curses\n  -n use -nographic (overrides -c)\n  -d more output\n  use -t to specify an alternative target directory for files\n\n  OS can be NetBSD, OpenBSD, FreeBSD, Plan9, Debian or Solaris\n"
 
 # Set the environment variable QEMUTARGET if you want an
 # alternative to $HOME/Qemu
@@ -26,6 +27,7 @@ OPENBSDCDN="https://cloudflare.cdn.openbsd.org/pub/OpenBSD"
 FREEBSDCDN="https://download.freebsd.org/ftp/releases"
 DEBIANCDN="https://cdimage.debian.org/debian-cd/current/"
 SOLARISCDN="" # Needs a login, can't download automagically
+PLAN9CDN="https://plan9.io/plan9/download"
 
 # Defaults
 DEBUG=0
@@ -34,19 +36,38 @@ ARCH=amd64
 SIZE=8G
 MEMORY=256M
 EXTRAFLAGS=""
-SETUP=""
+SETUP="0"
 
 NEEDISO="" # Need ISO for regular operation
 
-if [ "$1" = "-i" ]; then
-	# Setup
-	SETUP=1
-	shift
-fi
+# brew version seems to support curses. Perhaps need to recompile
+# -nographic doesn't work with NetBSD 
+#CURSES="-display curses"
+CURSES=""
 
 # Get the OS from the command-line
 #
-
+# CLI optiosn
+#
+while [ $# -gt 0 ]; do
+	case $1 in
+  -d|--debug)  DEBUG=1; ;;
+	-i|--install|--setup)				
+			SETUP="1"; ;;		# Run the installer
+	-c|--curses)
+			CURSES="-display curses"; ;;
+	-n|--nographic)				
+			CURSES="-nographic"; ;;
+	-t)			  QEMUTARGET="$2"; shift; ;;
+	-h|--help)		
+				echo "$USAGE"; exit ;;
+	-*)		echo "${0##*/}: unknown option \"$1\"" 1>&2
+			  echo "$USAGE" 1>&2; exit 1 ;;
+	 *)
+		    break; # Exit the while loop, rest of vars dealt with below
+	esac 
+	shift
+done
 
 if [ -n "$1" ]; then
 	OS=$1
@@ -61,7 +82,6 @@ if [ "$QEMUTARGET" = "" ]; then
 fi
 TARGET=$QEMUTARGET/$OS
 
-
 # Determine the architecture
 #
 if [ -n "$2" ]; then
@@ -69,21 +89,45 @@ if [ -n "$2" ]; then
 fi
 
 EMU=$ARCH
-
-# brew version seems to support curses. Perhaps need to recompile
-# -nographic doesn't work with NetBSD 
-#CURSES="-display curses"
-CURSES=""
-
 IMAGE="$LOWEROS-disk-$ARCH.img"
+
+# Use the correct emulator for the architecture
+#
+case $ARCH in
+		amd64)
+			EMU="x86_64"
+			;;
+		macppc|powerpc)
+			EMU="ppc"
+			;;
+esac
+
+
 # Fix depending on OS and arch
+#
 case $OS in
-  Solaris|Slowaris)
+  Plan9)
+	  VERS=latest
+		SIZE=4G
+		case $ARCH in
+			amd64)
+			;;
+			*)
+			echo "$OS/$ARCH not supported">&2
+			exit 1
+			;;
+		esac
+	;;
+	
+	Solaris|Slowaris)
 	  OS=Solaris # Sorry everyone. I did enjoy using it for work years ago :-)
-		VERS=10   
 		MEMORY=1G 		# Memory hungry
 		case $ARCH in
 			i386)
+			VERS=10
+			;;
+			amd64)
+			VERS=11
 			;;
 			*)
 			echo "$OS/$ARCH not supported">&2
@@ -101,16 +145,8 @@ case $OS in
 #				EXTRAFLAGS="-nographic" # XXX not sure this is needed
 				;;
 			amd64)
-				EMU="x86_64"
 				;;
-#			arm64)
-#			if [ "$SETUP" = "1" ]; then
-#				echo "Use setup-arm64.sh">&2 # XXX we should really setup here
-#				exit 1
-#			fi
-#				;;
 			macppc)
-				EMU="ppc"
 				VERS=9.0	# 9.1 doesn't boot
 				
 				echo "### Warning: 9.1 does not work (uses 9.0 by default)">&2
@@ -128,11 +164,8 @@ case $OS in
 	OpenBSD)
   	VERS=6.8
 		case $ARCH in
-			i386|sparc64)
+			i386|sparc64|amd64)
 				# Supported for OpenBSD
-				;;
-			amd64)
-				EMU="x86_64"
 				;;
 			*)
 				echo "$OS/$ARCH not supported">&2
@@ -143,12 +176,15 @@ case $OS in
 		FreeBSD)
 	  	VERS=12.2
 			case $ARCH in
-			i386|sparc64)
+			i386|amd64)
 					# Supported for FreeBSD
 					;;
-				amd64)
-					EMU="x86_64"
+				sparc64)
+				  # Has trouble without this
+					echo "Using -nographic for FreeBSD">&2
+					CURSES="-nographic";
 					;;
+					
 				*)
 					echo "$OS/$ARCH not supported">&2
 					exit 1
@@ -161,7 +197,6 @@ case $OS in
 			case $ARCH in
 				amd64)
 					# Debian
-					EMU="x86_64"
 					MEMORY=1G 		# Installer grumbles about memory
 					;;
 				*)
@@ -172,7 +207,7 @@ case $OS in
 			;;
 		
   *)
-		echo "Supported OSes: NetBSD, OpenBSD, FreeBSD, Debian, Solaris">&2
+		echo "$USAGE">&2
 		exit 1
 		;;
 esac
@@ -185,12 +220,20 @@ fi
 # Operating system specifics across the architectures
 #
 case $OS in
+	Plan9)
+	ISO=plan9.iso
+	URL="$PLAN9CDN/$ISO.bz2"
+	BUNZIPISO="1"
+	;;
+	
 	Solaris)
+		
 	ARCH1=x86
 	if [ "$ARCH" = "sparc64" ]; then 
 		ARCH1=sparc # This isn't used but is here for completeness
 	fi
 	ISO="sol-$VERS-u11-ga-$ARCH1-dvd.iso"
+	[ "$VERS" = "11" ] && ISO="sol-11_4-text-$ARCH1.iso"
 	URL="" #Not used for Solaris
 	;;
 	NetBSD)
@@ -246,7 +289,7 @@ fi
 case $OS in
 	Solaris)
 		if [ ! -f "$ISO" ]; then
-			echo "### Please download the ISOs from Oracle">&2
+			echo "### Please download the ISO: $ISO from Oracle">&2
 			echo "### You will need a login">&2
 			echo "### Place them in $FINALTARGET">&2
 			exit 1
@@ -259,12 +302,12 @@ case $OS in
 	else
 		echo "Creating $IMAGE">&2
 		qemu-img create -f raw "$IMAGE" $SIZE
-		[ "$SETUP" != "1" ] && echo "Changing to Setup mode">&2
-		SETUP=1
+		echo "(Using Setup mode)">&2
+		SETUP="1"
 	fi
-	BOOT="c"
+	BOOT="-boot c"
 	if [ "$SETUP" = "1" ] ; then
-		BOOT="d"
+		BOOT="-boot d"
 	fi
 
 if [ -f "$ISO" ]; then
@@ -273,27 +316,34 @@ else
 	
 	if [ "$SETUP" = "1" ] || [ "$NEEDISO" = 1 ]; then
 	
+	  INTERMEDIATE="$ISO"
+		if [ "$BUNZIPISO" = "1" ]; then
+			INTERMEDIATE="$ISO.bz2"
+		fi
   	echo "Downloading $ISO">&2	
-		echo "curl --location --output \"$ISO\" \"$URL\""
+		echo "curl --location --output \"$INTERMEDIATE\" \"$URL\""
 	
-		curl --location --output $ISO "$URL"
+		curl --location --output $INTERMEDIATE "$URL"
+		
+		if [ "$BUNZIPISO" = "1" ]; then
+			bunzip2 $INTERMEDIATE
+		fi
+		
 	fi
 fi
 
-
 INSTALLFLAGS=""
-
 
 case $ARCH in
 	i386|amd64)
-	QEMUFLAGS="-m $MEMORY -hda $IMAGE $CURSES -net user -net nic"
+	QEMUFLAGS="-m $MEMORY -hda $IMAGE -net user -net nic"
 	[ "$SETUP" = "1" ] && INSTALLFLAGS="-cdrom $ISO"
   ;;
   macppc|powerpc)
 	# I need the ISO to boot from after installation
 	#
-	[ "$SETUP" = "" ] && echo "### At Boot:  type  netbsd.macppc -a">&2
-	[ "$SETUP" = "$1" ] && OFWBOOT="-prom-env boot-device=cd:,\\ofwboot.xcf"
+	[ "$SETUP" = "0" ] && echo "### At Boot:  type  netbsd.macppc -a">&2
+	[ "$SETUP" = "1" ] && OFWBOOT="-prom-env boot-device=cd:,\\ofwboot.xcf"
 	QEMUFLAGS="$OFWBOOT $IMAGE" 
 	INSTALLFLAGS="-cdrom $ISO" # CD is needed for regular running...
   ;;
@@ -311,7 +361,11 @@ case $ARCH in
 	;;
 esac
 
+echo "#!/bin/sh" >boot.sh
+echo "# This is an experiment" >>boot.sh
+echo "qemu-system-$EMU $EXTRAFLAGS $CURSES $QEMUFLAGS -boot c" >>boot.sh
+
 echo "Starting emulator"
-echo "qemu-system-$EMU $EXTRAFLAGS $QEMUFLAGS $INSTALLFLAGS"
+echo "qemu-system-$EMU $EXTRAFLAGS $CURSES $QEMUFLAGS $INSTALLFLAGS $BOOT"
 sleep 2
-qemu-system-$EMU $EXTRAFLAGS $QEMUFLAGS $INSTALLFLAGS -boot $BOOT
+qemu-system-$EMU $EXTRAFLAGS $CURSES $QEMUFLAGS $INSTALLFLAGS $BOOT
